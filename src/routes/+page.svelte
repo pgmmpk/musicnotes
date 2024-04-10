a67dc214a85eb4f6a2f90a56ca9177fbe77ae752
Mike Kroutikov <pgmmpk@gmail.com>
2024-04-10 16:33:44 -0400

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -154,11 +154,11 @@
 
 <div class="flex flex-col h-screen align-center items-center unselectable">
     <div class="flex-none flex flex-row justify-center w-screen flex-wrap mx-1">
-        <div class="flex-none shrink-0 w-72 items-center flex-cols flex">
+        <div class="flex-none shrink-0 w-64 items-center flex-cols flex">
             <span class="mx-2 text-xl">{num}</span>
             <input class="outline-none p-1 bg-gray-200 grow mr-4 accent-gray-700 rounded-full border border-gray-700 appearance-none cursor-pointer" type="range" bind:value={num} min="3" max="8">
         </div>
-        <div class="flex-none shrink-0 w-72 items-center justify-center mx-1 flex-cols flex">
+        <div class="flex-none shrink-0 w-64 items-center justify-center mx-1 flex-cols flex">
             <button class="flex-none w-12 m-4 text-gray-700 disabled:text-gray-400 outline-none" onclick={repeat} disabled={score.length === 0}>
                 <svg class="w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"/>
